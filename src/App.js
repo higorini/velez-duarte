419df9dcbc65f40f9e4f6b6ad9e362de6cfd51ac
higorini <108697425+higorini@users.noreply.github.com>
2024-08-01 22:48:36 +0100

    feat: header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Header from "./components/Header/Header";
 
 function App() {
-  return <h1>Hello, World!</h1>;
+  return (
+    <>
+      <Header />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,55 @@
+import React from "react";
+import "./header.css";
+
+const nav__links = [
+  {
+    path: "#home",
+    display: "Home",
+  },
+  {
+    path: "#about",
+    display: "Sobre",
+  },
+  {
+    path: "#service",
+    display: "Serviços",
+  },
+  {
+    path: "#projetcs",
+    display: "Projetos",
+  },
+];
+
+const Header = () => {
+  return (
+    <header className="header">
+      <div className="container">
+        <div className="nav__wrapper">
+          <div className="logo">
+            <h2>Velez & Duarte</h2>
+          </div>
+
+          <div className="navigation">
+            <ul className="menu">
+              {nav__links.map((item) => (
+                <li className="menu__item">
+                  <a href={item.path} className="menu__link">
+                    {item.display}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+
+          <div className="light__mode">
+            <span>
+              <i className="ri-sun-line"></i> Modo Claro
+            </span>
+          </div>
+        </div>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
